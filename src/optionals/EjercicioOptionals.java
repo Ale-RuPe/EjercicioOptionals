3757f6ac882af14b4f223a604c9595a1a52aa6ef
Alejandro Ruiz Perez <alejandro.perez1@ibm.com>
2020-04-15 20:33:05 -0500

    nullSafeFindCheapestInsurance corregido

--- src/optionals/EjercicioOptionals.java
+++ src/optionals/EjercicioOptionals.java
@@ -2,7 +2,6 @@ package optionals;
 
 import java.util.Optional;
 import java.util.Properties;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class EjercicioOptionals {
@@ -16,45 +15,66 @@ public class EjercicioOptionals {
 		
 		System.out.println( main.getCarInsuranceName( Optional.of(person), 18 ) );
 		
-		car= null;
+		car = null;
+		Optional<Person> p = Optional.ofNullable(person);
 		Optional<Car> c = Optional.ofNullable(car);
-		System.out.println( main.nullSafeFindCheapestInsurance(Optional.of(person), c).isPresent() );		
+		System.out.println( "NS:"+main.nullSafeFindCheapestInsurance(p, c).isPresent() );
+		
 	}
 	
 	public String getCarInsuranceName(Optional<Person> person, int minAge) {
 		//return p.getCar().getInsurance().getName();
 		
 		return person
-				.filter( (p) -> p.getAge() >= minAge )
-				.flatMap(Person::getCar)
-				.flatMap(Car::getInsurance)
-				.map(Insurance::getName)
-				.orElse("Unknown");
+			.filter( (p) -> p.getAge() >= minAge )
+			.flatMap(Person::getCar)
+			.flatMap(Car::getInsurance)
+			.map(Insurance::getName)
+			.orElse("Unknown");
 	}
 	
-	Insurance findCheapestInsurance(Person p, Car c) {
+	public Insurance findCheapestInsurance(Person p, Car c) {
 		// logica blablabla
-		Insurance cheapestCompany = new Insurance("Compañia Barata");
+		Insurance cheapestCompany = new Insurance("Compania Barata");
 		return cheapestCompany;
 	}
 	
-	public Optional<Insurance> nullSafeFindCheapestInsurance(Optional<Person> p, Optional<Car> c) {
-		if( p.isPresent() && c.isPresent()) {
-			return Optional.of( findCheapestInsurance(p.get(), c.get()) );
-		}
-		else {
-			return Optional.empty();
-		}
+	public Optional<Insurance> nullSafeFindCheapestInsurance(Optional<Person> person, Optional<Car> car) {
+//		if( person.isPresent() && car.isPresent()) {
+//			return Optional.of( findCheapestInsurance(person.get(), car.get()) );
+//		}
+//		else {
+//			return Optional.empty();
+//		}	
+		
+		return person
+				.filter( p-> p.getCar().equals(car) )
+				.flatMap( Person::getCar )
+				.map( c -> findCheapestInsurance(person.get(), c) );
 	}
 	
 	public int readDuration(Properties prop, String name) {		
 		return Stream.of(prop)
 			.filter(p->p.containsKey(name))
 			.filter(p -> !p.getProperty(name).equals(null))
-			.filter(  p->Pattern.matches("(-?[0-9]*)",p.getProperty(name)) )
+			.filter(  p-> this.isNumeric(p.getProperty(name)) )
 			.map( p-> p.getProperty(name)  )
-			.map(Integer::parseInt)
+			.map( Integer::parseInt )
 			.findFirst()
 			.orElse(0);
 	}
+	
+	public boolean isNumeric(String cadena) {
+        boolean resultado;
+
+        try {
+            Integer.parseInt(cadena);
+            resultado = true;
+        } catch (NumberFormatException excepcion) {
+            resultado = false;
+        }
+
+        return resultado;
+    }
+	
 }
